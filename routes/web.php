b14a2372675762e57791cd67e7c747726fee3be3
Indra Ardika <indraardika@gmail.com>
2025-08-06 16:06:21 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('sadarin2');
+    return view('homepage');
 });
 
 Route::get('/login/admin', function () {
