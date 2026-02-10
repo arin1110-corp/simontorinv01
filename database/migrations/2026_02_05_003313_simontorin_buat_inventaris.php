eb9d4deeaba0d5da961b43142fe5e3ba996ec957
Indra Ardika <indraardika@gmail.com>
2026-02-10 19:29:06 +0800

    baru

--- database/migrations/2026_02_05_003313_simontorin_buat_inventaris.php
+++ database/migrations/2026_02_05_003313_simontorin_buat_inventaris.php
@@ -17,10 +17,17 @@ return new class extends Migration
         Schema::create('simontorin_inventaris', function (Blueprint $table) {
             $table->increments('inventaris_id');
             $table->string('inventaris_kode', 50)->unique();
+            $table->string('inventaris_kode_registerbarang', 50);
+            $table->string('inventaris_nomor_register', 50);
             $table->string('inventaris_nama', 150);
+            $table->string('inventaris_merk', 100);
+            $table->string('inventaris_model', 100);
             $table->string('inventaris_jenis', 150);
-            $table->date('inventaris_tanggal');
+            $table->string('inventaris_tahun_perolehan', 4);
             $table->string('inventaris_barcode', 100)->unique();
+            $table->string('inventaris_asalusul', 150);
+            $table->text('inventaris_keterangan');
+            $table->string('inventaris_kondisi', 50);
             $table->string('inventaris_status', 50);
         });
     }
